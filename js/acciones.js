3c6b09963a19d8ea12320b71265ba7934e0edf59
Enevi Aracely Pérez Hernández <cbtis16.perez.enevi.h@gmail.com>
2019-05-21 14:26:27 -0500

    4

--- js/acciones.js
+++ js/acciones.js
@@ -25,6 +25,7 @@ function getPosition(){
 		alert('Latitude:'  +position.coords.latitude  +'\n' + 
 		'Longitude:'  +position.coords.longitude  +'\n');
 	};
+	
 	function onError(error){
 		alert('code'  +error.code  +'\n'+'message:' +error.message +'\n');
 	}
