c25b637fc60d6cb5042b570d294bac3030953a20
Enevi Aracely Pérez Hernández <cbtis16.perez.enevi.h@gmail.com>
2019-05-21 14:17:07 -0500

    3

--- js/acciones.js
+++ js/acciones.js
@@ -20,12 +20,12 @@ function getPosition(){
 	
 	var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError,options);
 	
-	function onSuccess(posicion){
+	function onSuccess(position){
 		
-		alert('Latitude:' +position.coords.latitude +'\n' + 
-		'Longitude:' +position.coords.longitude +'\n');
+		alert('Latitude:'  +position.coords.latitude  +'\n' + 
+		'Longitude:'  +position.coords.longitude  +'\n');
 	};
 	function onError(error){
-		alert('code' +error.code +'\n' + 'message:' +error.message + '\n');
+		alert('code'  +error.code  +'\n'+'message:' +error.message +'\n');
 	}
 }
